d297f7f6925d02508db52357ce2fe97dcc21bead
Siddhant-Baranwal <siddhantbr10@gmail.com>
2024-04-13 16:27:14 +0530

    adding checks while filling the form

--- src/Components/Demo.js
+++ src/Components/Demo.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import axios from 'axios';
 
 const Demo = () => {
   const BACKEND_URL = "https://webify-backend-hnli.onrender.com";
+  const [nam, setNam] = useState("");
+  const [phone, setPhone] = useState("");
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    if (name === 'nam') {
+      setNam  (value);
+    } else if (name === 'phone') {
+      setPhone(value);
+    }
+  };
+  useEffect(() => {
+    console.log(nam);
+  }, [nam]);
+
+  useEffect(() => {
+    console.log(phone);
+  }, [phone]);
   const submitHandler = (event) => {
     event.preventDefault();
     const name = event.target.name.value;
@@ -25,13 +42,13 @@ const Demo = () => {
       <form className='form' onSubmit={submitHandler}>
         <div>
         <span>Name: </span>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="nam" value={nam} onChange={changeHandler} id="name" />
         </div>
         <div>
         <span>Phone: </span>
-        <input type="number" name="phone" id="phone" />
+        <input type="number" name="phone" value={phone} onChange={changeHandler} id="phone" />
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={nam.length===0 || phone.length!==10}>Submit</button>
       </form>
     </div>
     </>
